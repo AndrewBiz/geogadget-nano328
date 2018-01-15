230552458f851310e62b0051a9b1337206d27a7e
Andrew Bizyaev <andrew.bizyaev@gmail.com>
2018-01-16 00:23:09 +0300

    bump version

--- src/gg_version.hpp
+++ src/gg_version.hpp
@@ -3,7 +3,7 @@
 
 //======================================================================
 //  Program: gg.cpp - GeoGadget based on UBLOX GPS receiver
-#define GG_VERSION "0.9.4"
+#define GG_VERSION "0.9.5"
 //======================================================================
 
 #endif
